c8f01449de6bc076c140411c8eb4d4a9fa432994
Oliver Sigge <osigge@mac.com>
2012-12-02 21:50:01 +0100

    Added Jasmine Unit Tests
    Added Jasmine (http://pivotal.github.com/jasmine/)
    Added Jasmine-jQuery (https://github.com/velesin/jasmine-jquery)

---
+++ spec/javascripts/SwipeSpec.js
@@ -0,0 +1,47 @@
+describe('Swipe', function() {
+
+  var swipe;
+
+  beforeEach(function(){
+    loadFixtures('../../../index.html');
+    swipe = new Swipe($('#slider').get(0));
+  });
+
+  it('should have been created without options', function(){
+    expect(swipe).not.toBeNull();
+  });
+  
+  it('should slide to index 4 on #slider with duration 0', function(){
+    swipe.slide(4, 0);
+    expect(swipe.getPos()).toBe(4);
+  });
+  
+  describe('when calling next', function(){
+    it('should move to the next index 1', function(){
+      swipe.next();
+      expect(swipe.getPos()).toBe(1);
+    });
+    
+    it('should move to the index 0 when calling next on the last index', function(){
+      swipe.slide(5, 0);
+      swipe.next();
+      expect(swipe.getPos()).toBe(0);
+    });
+  });
+  
+  describe('when calling prev', function(){
+    it('should not move to the prev index when there is none', function(){
+      swipe.prev();
+      expect(swipe.getPos()).toBe(0);
+    });
+  });
+  
+  xdescribe('when invoking touch event', function(){
+    //TODO: implement
+  });
+  
+  xdescribe('when handling automatic transition', function(){
+    //TODO: implement
+  });
+
+});
