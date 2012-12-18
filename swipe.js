e8602987d0d13643ce054540650da2eb37f513c1
Antti-Jussi Kovalainen <ajk@ajk.fi>
2012-12-18 21:38:01 +0200

    Add support for Opera 12 and above

--- swipe.js
+++ swipe.js
@@ -218,7 +218,8 @@ Swipe.prototype = {
       case 'touchend': this.onTouchEnd(e); break;
       case 'webkitTransitionEnd':
       case 'msTransitionEnd':
-      case 'oTransitionEnd':
+      case 'oTransitionEnd': // opera 11 and below
+      case 'otransitionend': // opera 12 (and above?)
       case 'transitionend': this.onTransitionEnd(e); break;
       case 'resize': this.setup(); break;
     }
